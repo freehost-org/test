7f37933927c26bc4d5f7e0e99446e545d70fc6b9
Arrow6 <48833849+Arrow6@users.noreply.github.com>
2020-04-07 18:57:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ async function main() {
     div.src = "https://www.youtube.com/embed/VsXCK_2DJzA";
     div.width = 560;
     div.height = 315;
+    div.frameborder = 0;
+    div.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
     let text = document.createTextNode(iframe);
     //div.appendChild(text);
     document.body.appendChild(div);
